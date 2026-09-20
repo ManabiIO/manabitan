7336dede3e986204c755bbcd1282ca285e6e9d50
Alex Ehlke <alex.ehlke@gmail.com>
2026-09-20 18:39:17 -0400

    review: qualify search draft refresh repair against pinned develop

---
+++ test/search-refresh-input.test.js
@@ -0,0 +1,30 @@
+/*
+ * Copyright (C) 2026  Yomitan Authors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+import {spawnSync} from 'node:child_process';
+import {fileURLToPath} from 'node:url';
+import {expect, test} from 'vitest';
+
+test('search refresh preserves draft input with the actual display methods', () => {
+    const file = fileURLToPath(new URL('util/search-refresh-cases.js', import.meta.url));
+    const result = spawnSync(process.execPath, ['--test', '--test-reporter=tap', file], {
+        encoding: 'utf8', timeout: 30000, maxBuffer: 4 * 1024 * 1024,
+    });
+    expect(result.error).toBeUndefined();
+    expect(result.status, `${result.stdout}\n${result.stderr}`).toBe(0);
+    expect(result.stdout).toMatch(/# fail 0\b/u);
+}, 35000);
