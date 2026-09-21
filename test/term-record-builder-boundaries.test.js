8486ed6cd7dff03c847f0626ab534dc0f4b933f4
Alex Ehlke <alex.ehlke@gmail.com>
2026-09-21 14:32:55 -0400

    test: cover preinterned row ID boundaries

---
+++ test/term-record-builder-boundaries.test.js
@@ -0,0 +1,16 @@
+/*
+ * Copyright (C) 2026 Manabitan authors
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+/* eslint @stylistic/semi: ["error", "never"] */
+
+import {execFileSync} from 'node:child_process'
+import {fileURLToPath} from 'node:url'
+import {test} from 'vitest'
+
+test('preinterned record builder rejects invalid IDs before narrowing', () => {
+    execFileSync(process.execPath, [
+        '--test',
+        fileURLToPath(new URL('./util/term-record-builder-boundary-cases.js', import.meta.url)),
+    ], {encoding: 'utf8', timeout: 30_000})
+})
