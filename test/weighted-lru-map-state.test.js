7750bd69c563c122b3fa63bd7ddcd2f24cdaeab9
Alex Ehlke <alex.ehlke@gmail.com>
2026-09-21 15:09:48 -0400

    test: run weighted cache state regressions in the unit suite

---
+++ test/weighted-lru-map-state.test.js
@@ -0,0 +1,16 @@
+/*
+ * Copyright (C) 2026 Manabitan authors
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+/* eslint @stylistic/semi: ["error", "never"] */
+
+import {execFileSync} from 'node:child_process'
+import {fileURLToPath} from 'node:url'
+import {test} from 'vitest'
+
+test('weighted cache preserves recency, exception safety and reentrant accounting', () => {
+    execFileSync(process.execPath, [
+        '--test',
+        fileURLToPath(new URL('util/weighted-lru-map-state-cases.js', import.meta.url)),
+    ], {encoding: 'utf8', timeout: 30_000})
+})
