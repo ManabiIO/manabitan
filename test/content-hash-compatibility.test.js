8deebd1f5d3112ae4512e7954d3bd8a34aa3dcb6
Alex Ehlke <alex.ehlke@gmail.com>
2026-09-21 23:39:19 -0400

    test(hash): fix compatibility wrapper lint

--- test/content-hash-compatibility.test.js
+++ test/content-hash-compatibility.test.js
@@ -1,8 +1,8 @@
-import {readFileSync} from 'node:fs'
-import {test} from 'vitest'
-import {parseJson} from '../ext/js/core/json.js'
-import * as hash from '../ext/js/dictionary/term-entry-content-hash.js'
-import {registerContentHashCompatibilityCases} from './util/content-hash-compatibility-cases.js'
+import {readFileSync} from 'node:fs';
+import {test} from 'vitest';
+import {parseJson} from '../ext/js/core/json.js';
+import * as hash from '../ext/js/dictionary/term-entry-content-hash.js';
+import {registerContentHashCompatibilityCases} from './util/content-hash-compatibility-cases.js';
 
-const fixture = /** @type {{vectors: {length: number, salt: number, pair: number[]}[]}} */ (parseJson(readFileSync(new URL('./data/content-hash-native-vectors.json', import.meta.url), 'utf8')))
-registerContentHashCompatibilityCases(test, hash, fixture)
+const fixture = /** @type {{vectors: {length: number, salt: number, pair: number[]}[]}} */ (parseJson(readFileSync(new URL('data/content-hash-native-vectors.json', import.meta.url), 'utf8')));
+registerContentHashCompatibilityCases(test, hash, fixture);
