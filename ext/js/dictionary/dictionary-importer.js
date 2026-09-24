0bcfaf2584f3d45dc39406067e445bd219d5cee4
Alex Ehlke <alex.ehlke@gmail.com>
2026-09-24 13:08:57 -0400

    fix(import): avoid spread overflow for media requirements

--- ext/js/dictionary/dictionary-importer.js
+++ ext/js/dictionary/dictionary-importer.js
@@ -4098,7 +4098,9 @@ export class DictionaryImporter {
                 } else {
                     termList.push(...termListChunk);
                     if (requirements !== null && requirementsForChunk !== null) {
-                        requirements.push(...requirementsForChunk);
+                        for (const requirement of requirementsForChunk) {
+                            requirements.push(requirement);
+                        }
                     }
                 }
                 importerChunkSinkMs += Math.max(0, Date.now() - tChunkSinkStart);
